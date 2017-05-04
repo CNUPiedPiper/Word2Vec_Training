bfd9eec317b72ed8f5db71c55602e3828234dc88
Yoon-jae <yoonjae.cho92@gmail.com>
2017-05-04 14:13:26 +0900

    Cleanup code

--- train_pos_data.py
+++ train_pos_data.py
@@ -1,13 +1,9 @@
 
 # coding: utf-8
 
-# In[8]:
-
 import gensim
 import codecs
 
-
-# In[34]:
 
 class SentenceReader:
 
@@ -19,8 +15,6 @@ class SentenceReader:
             yield line.split(' ')
 
 
-# In[35]:
-
 sentences_vocab = SentenceReader('/Users/jegalsumin/Documents/HARU/output.txt')
 sentences_train = SentenceReader('/Users/jegalsumin/Documents/HARU/output.txt')
 
@@ -28,28 +22,10 @@ model = gensim.models.Word2Vec()
 model.build_vocab(sentences_vocab)
 model.train(sentences_train,total_examples=model.corpus_count,epochs=model.iter)
 
+''' Demo
+print(model.most_similar(positive=["남자/Noun", "여왕/Noun"], negative=["여자/Noun"], topn=1))
 
-# In[38]:
+print(model.most_similar('날씨/Noun'))
 
-model.most_similar(positive=["남자/Noun", "여왕/Noun"], negative=["여자/Noun"], topn=1)
-
-
-# In[39]:
-
-model.most_similar('날씨/Noun')
-
-
-# In[65]:
-
-model.most_similar(positive=['심상정/Noun','민주당/Noun'],negative=['문재인/Noun'],topn=1)
-
-
-# In[64]:
-
-model.vector_size
-
-
-# In[ ]:
-
-
-
+print(model.most_similar(positive=['심상정/Noun','민주당/Noun'],negative=['문재인/Noun'],topn=1))
+'''
